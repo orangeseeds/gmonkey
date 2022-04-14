b36646610aff648b7cf539d010c81e84837a7beb
orangeseeds <shubhechchhakp@gmail.com>
2022-04-14 20:19:41 +0545

    basic REPL implementation for gmonkey

---
+++ repl/repl.go
@@ -0,0 +1,35 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/orangeseeds/gmonkey/lexer"
+	"github.com/orangeseeds/gmonkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf("%v", PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		if line == "exit" {
+			os.Exit(1)
+		}
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
